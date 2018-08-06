8a38258926fa8ccfa6791a428cff6fb659d9040d
TazdingoHuang <40261008+TazdingoHuang@users.noreply.github.com>
2018-08-06 14:11:39 +0800

    bug fixed

--- LasToIndexedCsv.py
+++ LasToIndexedCsv.py
@@ -2,21 +2,28 @@ from laspy.file import File
 import numpy as np
 import os
 
-def LasToCsv(lasFileName):
+def LasToCsv(lasFileName, shift = True):
     lasFile = File(lasFileName, mode='r')
     X = lasFile.X
     Y = lasFile.Y
     Z = lasFile.Z
+    if shift:
+        X = X - min(X)
+        Y = Y - min(Y)
+        Z = Z - min(Z)
     I = lasFile.intensity
-    index = np.arange(x.shape[0])
+    index = np.arange(X.shape[0])
     data = np.stack((X,Y,Z,I,index),axis=1)
     np.savetxt(lasFileName+".csv", data, fmt="%.3f,%.3f,%.3f,%d,%d")
 
-#put this .py file and .las files into same directory
+
+##put this .py file and .las files into same directory
+##LasToCsv(f, False) for not to shift
 for f in os.listdir(os.getcwd()):
     if f.endswith(".las"):
         print("current: " + f)
         LasToCsv(f)
+
 
 ## single file
 # lasFileName = "place/of/the/las/file.las"
